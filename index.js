2065dd0f70abc6789b56c79bf825486241b8055f
TarushGupta23 <tarush23.gupta@gmail.com>
2024-07-15 22:49:12 +0530

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: "*", // NOTE: change this to your website url.
+        origin: "https://atlas-game.vercel.app", // NOTE: change this to your website url.
         methods: ["GET", "POST"]
     }
 });
